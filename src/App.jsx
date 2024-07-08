51f1ae1e60ef8185a3ff8c4bb5711b0e3035f53d
Eduardo Maffissoni <studiogameplays@gmail.com>
2024-07-08 13:01:10 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 function App() {
   const [professionsObj, setProfessionsObj] = useState([]);
-  const [activeProfession, setActiveProfession] = useState(null);
+  const [activeProfession, setActiveProfession] = useState('');
+
   function fetchData(id) {
     fetch(
       ` https://us.api.blizzard.com/data/wow/profession/${id}?namespace=static-us&locale=en_US&access_token=USLyXrvnS8AFGShg2vvtFcsfWR8bbSR05B`
@@ -47,17 +48,24 @@ function App() {
       });
   }, []);
 
-  console.log(professionsObj);
-
   function handleClick(profession) {
-    setActiveProfession(profession);
+    setActiveProfession(() => profession);
+  }
+
+  function ActiveInfo({ prof }) {
+    const categoriesFlatten = prof.categories.map((cat) => cat.recipes).flat();
+    console.log(categoriesFlatten);
+    const recipesEl = categoriesFlatten.map((recipe) => (
+      <li key={recipe.id}>{recipe.name}</li>
+    ));
+    return <div>{recipesEl}</div>;
   }
 
   function Profession({ profession, handleClick }) {
     return (
       <button
-        className='p-6 bg-slate-300 hover:scale-110'
-        onClick={handleClick(profession)}
+        className='p-6 bg-slate-300 h-20 hover:scale-110 w-60'
+        onClick={() => handleClick(profession)}
       >
         {profession.name}
       </button>
@@ -68,7 +76,12 @@ function App() {
     <Profession profession={obj} handleClick={handleClick} key={obj.name} />
   ));
 
-  return <div className='bg-sky-400 h-screen w-full'>{professions}</div>;
+  return (
+    <div>
+      <div className='gap-4 flex'>{professions}</div>
+      {activeProfession ? <ActiveInfo prof={activeProfession} /> : ''}
+    </div>
+  );
 }
 
 export default App;
